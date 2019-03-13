ea25aebf39caca4e07c4a988aea2b7865778c611
charlie sievers <charliesievers@campus-058-165.ucdavis.edu>
2019-03-13 11:28:30 -0700

    Add Riccardo, Update undergrads, update publications

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,73 @@
-!function(e){skel.breakpoints({xlarge:"(max-width: 1680px)",large:"(max-width: 1280px)",medium:"(max-width: 980px)",small:"(max-width: 736px)",xsmall:"(max-width: 480px)"}),e(function(){var i=e(window),o=e("body"),n=e("#header");o.addClass("is-loading"),i.on("load",function(){window.setTimeout(function(){o.removeClass("is-loading")},100)}),e("form").placeholder(),skel.on("+medium -medium",function(){e.prioritize(".important\\28 medium\\29",skel.breakpoint("medium").active)}),e(".scrolly").scrolly({offset:function(){return n.outerHeight()}}),e("#menu").append('<a href="#menu" class="close"></a>').appendTo(o).panel({delay:500,hideOnClick:!0,hideOnSwipe:!0,resetScroll:!0,resetForms:!0,side:"right"}),i.on("resize",function(){i.trigger("scroll")}),o.scrollex({top:"25vh",mode:"top",enter:function(){n.addClass("alt")},leave:function(){n.removeClass("alt")}})})}(jQuery);
+/*
+	Hologram by Pixelarity
+	pixelarity.com | hello@pixelarity.com
+	License: pixelarity.com/license
+*/
+
+(function($) {
+
+	skel.breakpoints({
+		xlarge:	'(max-width: 1680px)',
+		large:	'(max-width: 1280px)',
+		medium:	'(max-width: 980px)',
+		small:	'(max-width: 736px)',
+		xsmall:	'(max-width: 480px)'
+	});
+
+	$(function() {
+
+		var	$window = $(window),
+			$body = $('body'),
+			$header = $('#header');
+
+		// Disable animations/transitions until the page has loaded.
+			$body.addClass('is-loading');
+
+			$window.on('load', function() {
+				window.setTimeout(function() {
+					$body.removeClass('is-loading');
+				}, 100);
+			});
+
+		// Fix: Placeholder polyfill.
+			$('form').placeholder();
+
+		// Prioritize "important" elements on medium.
+			skel.on('+medium -medium', function() {
+				$.prioritize(
+					'.important\\28 medium\\29',
+					skel.breakpoint('medium').active
+				);
+			});
+
+		// Scrolly.
+			$('.scrolly').scrolly({
+				offset: function() { return $header.outerHeight() }
+			});
+
+		// Menu.
+			$('#menu')
+				.append('<a href="#menu" class="close"></a>')
+				.appendTo($body)
+				.panel({
+					delay: 500,
+					hideOnClick: true,
+					hideOnSwipe: true,
+					resetScroll: true,
+					resetForms: true,
+					side: 'right'
+				});
+
+		// Header.
+			$window.on('resize', function() { $window.trigger('scroll'); });
+
+			$body.scrollex({
+				top:		'25vh',
+				mode:		'top',
+				enter:		function() { $header.addClass('alt'); },
+				leave:		function() { $header.removeClass('alt'); }
+			});
+
+	});
+
+})(jQuery);
